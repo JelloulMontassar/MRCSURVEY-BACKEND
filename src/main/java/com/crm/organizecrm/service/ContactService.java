2638e42146c120218ae23c5ce87bf4538d48c60f
Fahed M'barek <94365100+Fahedmb@users.noreply.github.com>
2024-07-05 23:31:05 +0200

    Refactoring ContactController ContactService ContactServiceImpl

--- src/main/java/com/crm/organizecrm/service/ContactService.java
+++ src/main/java/com/crm/organizecrm/service/ContactService.java
@@ -1,14 +1,14 @@
 package com.crm.organizecrm.service;
 
-import com.crm.organizecrm.model.Contact;
+import com.crm.organizecrm.dto.ContactDTO;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ContactService {
-    Contact createContact(Contact contact);
-    Contact updateContact(Long id, Contact contact);
+    ContactDTO createContact(ContactDTO contactDTO);
+    ContactDTO updateContact(Long id, ContactDTO contactDTO);
     void deleteContact(Long id);
-    Optional<Contact> getContactById(Long id);
-    List<Contact> getAllContacts();
+    Optional<ContactDTO> getContactById(Long id);
+    List<ContactDTO> getAllContacts();
 }
