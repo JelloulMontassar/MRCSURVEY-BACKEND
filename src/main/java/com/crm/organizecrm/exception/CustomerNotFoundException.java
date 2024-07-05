91b885438564f05a0bdf180f2a34af5c27b2e748
Fahed M'barek <94365100+Fahedmb@users.noreply.github.com>
2024-07-05 13:24:20 +0200

    deleted unused CustomerNotFoundException.java

--- src/main/java/com/crm/organizecrm/exception/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.crm.organizecrm.exception;
-
-public class CustomerNotFoundException extends RuntimeException{
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
