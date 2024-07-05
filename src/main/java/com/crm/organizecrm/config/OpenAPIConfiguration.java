bbbad73c6b54589762824ce5d0cccaab8cb4be94
JelloulMontassar <j3lloulm@gmail.com>
2024-07-05 21:22:40 +0100

    Fix

--- src/main/java/com/crm/organizecrm/config/OpenAPIConfiguration.java
+++ src/main/java/com/crm/organizecrm/config/OpenAPIConfiguration.java
@@ -4,19 +4,30 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.List;
 
 @Configuration
 public class OpenAPIConfiguration {
 
+    @Value("${base.url}")
+    private String baseUrl;
+
+    private final Environment environment;
+
+    public OpenAPIConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public OpenAPI defineOpenApi() {
         Server server = new Server();
-        server.setUrl("http://localhost:8080");
-        server.setDescription("Development");
+        server.setUrl(baseUrl);
+        server.setDescription(getServerDescription());
 
         Contact myContact = new Contact();
         myContact.setName("Jane Doe");
@@ -27,6 +38,16 @@ public class OpenAPIConfiguration {
                 .version("1.0")
                 .description("This API exposes endpoints to manage employees.")
                 .contact(myContact);
+
         return new OpenAPI().info(information).servers(List.of(server));
+    }
+
+    private String getServerDescription() {
+        String[] activeProfiles = environment.getActiveProfiles();
+        if (activeProfiles.length > 0) {
+            return activeProfiles[0] + " server";
+        } else {
+            return "Local server";
+        }
     }
 }
