176ece25de352dbe114fd330dfe1ea2e984b8e55
JelloulMontassar <j3lloulm@gmail.com>
2024-07-06 15:51:29 +0100

    Fix Build

--- src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
+++ src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
@@ -1,13 +1,17 @@
 package com.crm.organizecrm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrganizeCrmApplicationTests {
+	@Value("${spring.datasource.url}")
+	private String url;
 
 	@Test
 	void contextLoads() {
+		System.out.println(url);
 	}
 
 }
