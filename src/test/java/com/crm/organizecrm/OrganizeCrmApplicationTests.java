7cbcc82219859f2f4ae9275baa0e25c6298ce585
JelloulMontassar <j3lloulm@gmail.com>
2024-07-06 18:42:32 +0100

    Fix Build

--- src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
+++ src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
@@ -1,5 +1,6 @@
 package com.crm.organizecrm;
 
+import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,12 @@ class OrganizeCrmApplicationTests {
 
 	@Value("${spring.datasource.password}")
 	private String password;
-
+	@PostConstruct
+	void init() {
+		System.out.println("DATABASE URL: " + url);
+		System.out.println("DATABASE USERNAME: " + username);
+		System.out.println("DATABASE PASSWORD: " + password);
+	}
 	@Test
 	void contextLoads() {
 		System.out.println("DATABASE URL: " + url);
