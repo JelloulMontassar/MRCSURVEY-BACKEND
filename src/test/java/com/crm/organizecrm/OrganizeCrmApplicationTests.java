6f0f25205fe48b56d0463d8fb0b869a32181dbb0
JelloulMontassar <j3lloulm@gmail.com>
2024-07-06 20:11:46 +0100

    Fix Build

--- src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
+++ src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
@@ -3,6 +3,7 @@ package com.crm.organizecrm;
 import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
