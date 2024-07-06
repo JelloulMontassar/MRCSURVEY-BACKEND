103e6fe76e7bf3e1c1d3d29a6dbe3125d3354615
JelloulMontassar <j3lloulm@gmail.com>
2024-07-06 23:20:32 +0100

    Fix Build

--- src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
+++ src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
@@ -1,10 +1,5 @@
 package com.crm.organizecrm;
-
-import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -13,24 +8,9 @@ import org.springframework.test.context.ActiveProfiles;
 
 class OrganizeCrmApplicationTests {
 
-	@Value("${host}")
-	private String url;
 
-	@Value("${spring.datasource.username}")
-	private String username;
-
-	@Value("${spring.datasource.password}")
-	private String password;
-	@PostConstruct
-	void init() {
-		System.out.println("DATABASE URL: " + url);
-		System.out.println("DATABASE USERNAME: " + username);
-		System.out.println("DATABASE PASSWORD: " + password);
-	}
 	@Test
 	void contextLoads() {
-		System.out.println("DATABASE URL: " + url);
-		System.out.println("DATABASE USERNAME: " + username);
-		System.out.println("DATABASE PASSWORD: " + password);
+
 	}
 }
