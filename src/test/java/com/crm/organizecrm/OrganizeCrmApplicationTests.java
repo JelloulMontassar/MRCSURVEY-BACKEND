1c35158bad271b64acc8c38492b6a8d97e565dbf
JelloulMontassar <j3lloulm@gmail.com>
2024-07-06 16:02:03 +0100

    Fix Build

--- src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
+++ src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
@@ -6,13 +6,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrganizeCrmApplicationTests {
+
 	@Value("${spring.datasource.url}")
 	private String url;
 
+	@Value("${spring.datasource.username}")
+	private String username;
+
+	@Value("${spring.datasource.password}")
+	private String password;
+
 	@Test
 	void contextLoads() {
-		System.out.println("DATABASE URL");
+		System.out.println("DATABASE URL: " + url);
+		System.out.println("DATABASE USERNAME: " + username);
+		System.out.println("DATABASE PASSWORD: " + password);
 	}
-
-
 }
