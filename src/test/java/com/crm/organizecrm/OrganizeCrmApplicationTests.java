5376df7dd891b8c2eead5f2f40fd9d5cf9b1a781
JelloulMontassar <j3lloulm@gmail.com>
2024-07-07 20:57:16 +0100

    Fix Test

--- src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
+++ src/test/java/com/crm/organizecrm/OrganizeCrmApplicationTests.java
@@ -1,11 +1,12 @@
 package com.crm.organizecrm;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("ci")
-
+@AutoConfigureTestDatabase
 class OrganizeCrmApplicationTests {
 
 
